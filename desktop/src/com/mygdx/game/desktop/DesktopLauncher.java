69c73e49fd4fe36471b378e66491977f8e827eb2
andrewsi10 <andrewsi10@gmail.com>
2015-05-14 01:16:22 -0700

    Updates: changed default resolution of the game when it initializes.

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -4,13 +4,15 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.mygdx.game.Malice;
 
-//test
-
 public class DesktopLauncher
 {
-    public static void main( String[] arg )
-    {
-        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-        new LwjglApplication( new Malice(), config );
-    }
+	public static void main(String[] arg)
+	{
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = Malice.TITLE + " v" + Malice.VERSION;
+		config.vSyncEnabled = true;
+		config.width = 1600;
+		config.height = 900;
+		new LwjglApplication( new Malice(), config );
+	}
 }
