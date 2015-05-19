d907b4cb57a205f62c1881ddd53a893f4d6492fc
Christopher Cheung <cchrism0426@gmail.com>
2015-05-19 11:45:53 -0700

    Changed color of background
    black

--- core/src/com/mygdx/game/player/WarriorWalking.java
+++ core/src/com/mygdx/game/player/WarriorWalking.java
@@ -64,7 +64,7 @@ public class WarriorWalking implements Screen
 	@Override
 	public void render(float delta)
 	{
-		Gdx.gl.glClearColor( 1, 1, 1, 1 );
+		Gdx.gl.glClearColor( 0, 0, 0, 1 );
 		Gdx.gl.glClear( GL30.GL_COLOR_BUFFER_BIT );
 
 		if ( currentFrame < 29 )
