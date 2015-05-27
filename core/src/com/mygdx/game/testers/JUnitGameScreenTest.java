8e83354450c1167324f8651b92539fbd48397e18
SomPat <spathak701@LHS-PRLAB-18.lhs.win.fuhsd.org>
2015-05-27 12:45:18 -0700

    Game Screen Test

--- core/src/com/mygdx/game/testers/JUnitGameScreenTest.java
+++ core/src/com/mygdx/game/testers/JUnitGameScreenTest.java
@@ -1,5 +1,26 @@
 package com.mygdx.game.testers;
+import static org.junit.Assert.*;
+import junit.framework.JUnit4TestAdapter;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.mygdx.game.world.Map;
 
 public class JUnitGameScreenTest {
+	
+	
+	
+	
+	
+	public static junit.framework.Test suite()
+    {
+        return new JUnit4TestAdapter( JUnitMapTest.class );
+    }
+    
+    public static void main( String[] args )
+    {
+        org.junit.runner.JUnitCore.main( "JUnitMapTester" );
+    }
 
 }
