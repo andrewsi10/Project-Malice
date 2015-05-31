c4fecb6ac470ec37068684dccc8fc0d2fbc43c37
Christopher Cheung <cchrism0426@gmail.com>
2015-05-31 15:56:31 -0700

    Added Switch Characters button
    Added to GameOver screen

--- core/src/com/mygdx/game/screens/GameOver.java
+++ core/src/com/mygdx/game/screens/GameOver.java
@@ -24,15 +24,14 @@ import com.badlogic.gdx.scenes.scene2d.utils.SpriteDrawable;
 import com.mygdx.game.Malice;
 import com.mygdx.game.player.Player;
 
-public class GameOver implements Screen
-{
+public class GameOver implements Screen {
 	Image background;
 
 	private final Malice game;
 
 	private Stage stage;
 
-	private TextButton retryButton, exitButton;
+	private TextButton retryButton, exitButton, switchButton;
 
 	private Skin skin;
 
@@ -41,15 +40,14 @@ public class GameOver implements Screen
 	private BitmapFont font;
 
 	private Batch batch;
-	
+
 	private String playerType;
 
 	private GlyphLayout layout;
-	
+
 	Music music;
 
-	public GameOver(Malice g, Music m, Player player, String playerType)
-	{
+	public GameOver(Malice g, Music m, Player player, String playerType) {
 		game = g;
 		music = m;
 		this.player = player;
@@ -58,42 +56,39 @@ public class GameOver implements Screen
 	}
 
 	// copied from online
-	private void createSkin()
-	{
+	private void createSkin() {
 		// Create a font
 		BitmapFont font = new BitmapFont();
 
 		batch = new SpriteBatch();
 
-		background = new Image( (Drawable) new SpriteDrawable( new Sprite(
-				new Texture( "img/titlescreen.png" ) ) ) );
+		background = new Image((Drawable) new SpriteDrawable(new Sprite(
+				new Texture("img/titlescreen.png"))));
 
-		skin = new Skin( Gdx.files.internal( "ui/uiskin.json" ) );
-		skin.add( "default", font );
+		skin = new Skin(Gdx.files.internal("ui/uiskin.json"));
+		skin.add("default", font);
 
 		// Create a texture
-		Pixmap pixmap = new Pixmap( (int) Gdx.graphics.getWidth() / 4,
-				(int) Gdx.graphics.getHeight() / 10, Pixmap.Format.RGB888 );
-		pixmap.setColor( Color.WHITE );
+		Pixmap pixmap = new Pixmap((int) Gdx.graphics.getWidth() / 4,
+				(int) Gdx.graphics.getHeight() / 10, Pixmap.Format.RGB888);
+		pixmap.setColor(Color.WHITE);
 		pixmap.fill();
-		skin.add( "background", new Texture( pixmap ) );
+		skin.add("background", new Texture(pixmap));
 
 		// Create a button style
 		TextButton.TextButtonStyle textButtonStyle = new TextButton.TextButtonStyle();
-		textButtonStyle.up = skin.newDrawable( "background", Color.GRAY );
-		textButtonStyle.down = skin.newDrawable( "background", Color.DARK_GRAY );
-		textButtonStyle.checked = skin.newDrawable( "background",
-				Color.DARK_GRAY );
-		textButtonStyle.over = skin
-				.newDrawable( "background", Color.LIGHT_GRAY );
-		textButtonStyle.font = skin.getFont( "default" );
-		skin.add( "default", textButtonStyle );
+		textButtonStyle.up = skin.newDrawable("background", Color.GRAY);
+		textButtonStyle.down = skin.newDrawable("background", Color.DARK_GRAY);
+		textButtonStyle.checked = skin.newDrawable("background",
+				Color.DARK_GRAY);
+		textButtonStyle.over = skin.newDrawable("background", Color.LIGHT_GRAY);
+		textButtonStyle.font = skin.getFont("default");
+		skin.add("default", textButtonStyle);
 
 	}
 
 	@Override
-	public void show()
-	{
+	public void show() {
 		// Stage stage = new Stage();
 		// // atlas = new TextureAtlas( "ui/button.pack" );
 		// skin = new Skin( Gdx.files.internal( "ui/uiskin.json" ) );
@@ -109,98 +104,105 @@ public class GameOver implements Screen
 		// black = new BitmapFont( Gdx.files.internal( "fonts/black.fnt" ),
 		// false );
 
-		music.setVolume( 0.7f );
+		music.setVolume(0.7f);
 		stage = new Stage();
 		font = new BitmapFont();
-		font.setColor( Color.WHITE );
-		Gdx.input.setInputProcessor( stage );// Make the stage consume events
+		font.setColor(Color.WHITE);
+		Gdx.input.setInputProcessor(stage);// Make the stage consume events
 
 		createSkin();
-		retryButton = new TextButton( "Try Again", skin ); // Use
+		retryButton = new TextButton("Try Again", skin); // Use
 															// the
 															// initialized
 															// skin
 		retryButton.setPosition(
 				Gdx.graphics.getWidth() / 2 - Gdx.graphics.getWidth() / 8,
-				Gdx.graphics.getHeight() / 2 );
-		retryButton.addListener( new ClickListener()
-		{
+				Gdx.graphics.getHeight() / 2);
+		retryButton.addListener(new ClickListener() {
 			@Override
-			public void clicked(InputEvent event, float x, float y)
-			{
+			public void clicked(InputEvent event, float x, float y) {
 				retryButton.remove();
+				switchButton.remove();
 				exitButton.remove();
 				music.play();
-				game.setScreen( new GameScreen( game, music, playerType ) );
+				game.setScreen(new GameScreen(game, music, playerType));
 			}
-		} );
+		});
 
-		exitButton = new TextButton( "Exit", skin );
+		switchButton = new TextButton("Switch Characters", skin);
+		switchButton.setPosition(
+				Gdx.graphics.getWidth() / 2 - Gdx.graphics.getWidth() / 8,
+				Gdx.graphics.getHeight() / 3);
+		switchButton.addListener(new ClickListener() {
+			@Override
+			public void clicked(InputEvent event, float x, float y) {
+				retryButton.remove();
+				switchButton.remove();
+				exitButton.remove();
+				music.play();
+				game.setScreen(new CharacterSelect(game, music));
+			}
+		});
+		
+		exitButton = new TextButton("Exit", skin);
 		exitButton.setPosition(
 				Gdx.graphics.getWidth() / 2 - Gdx.graphics.getWidth() / 8,
-				Gdx.graphics.getHeight() / 4 );
-		exitButton.addListener( new ClickListener()
-		{
+				Gdx.graphics.getHeight() / 6);
+		exitButton.addListener(new ClickListener() {
 			@Override
-			public void clicked(InputEvent event, float x, float y)
-			{
+			public void clicked(InputEvent event, float x, float y) {
 				Gdx.app.exit();
 			}
-		} );
+		});
 
-		stage.addActor( background );
-		stage.addActor( retryButton );
-		stage.addActor( exitButton );
+		stage.addActor(background);
+		stage.addActor(retryButton);
+		stage.addActor(switchButton);
+		stage.addActor(exitButton);
 	}
 
 	@Override
-	public void render(float delta)
-	{
-		Gdx.gl.glClearColor( 0, 0, 0, 1 );
-		Gdx.gl.glClear( GL30.GL_COLOR_BUFFER_BIT );
+	public void render(float delta) {
+		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClear(GL30.GL_COLOR_BUFFER_BIT);
 		stage.act();
 		stage.draw();
 		batch.begin();
 		String str = "You earned " + player.getPoints()
-				+ " points and reached level " + player.getCurrentLevel() + ". Better luck next time!";
-		layout.setText( font, str );
-		font.draw( batch, str,
-				Gdx.graphics.getWidth() / 2 - layout.width / 2,
-				Gdx.graphics.getHeight() * 67 / 100 );
+				+ " points and reached level " + player.getCurrentLevel()
+				+ ". Better luck next time!";
+		layout.setText(font, str);
+		font.draw(batch, str, Gdx.graphics.getWidth() / 2 - layout.width / 2,
+				Gdx.graphics.getHeight() * 67 / 100);
 		batch.end();
 	}
 
 	@Override
-	public void resize(int width, int height)
-	{
+	public void resize(int width, int height) {
 		// TODO Auto-generated method stub
 
 	}
 
 	@Override
-	public void pause()
-	{
+	public void pause() {
 		// TODO Auto-generated method stub
 
 	}
 
 	@Override
-	public void resume()
-	{
+	public void resume() {
 		// TODO Auto-generated method stub
 
 	}
 
 	@Override
-	public void hide()
-	{
+	public void hide() {
 		// TODO Auto-generated method stub
 
 	}
 
 	@Override
-	public void dispose()
-	{
+	public void dispose() {
 		skin.dispose();
 		stage.dispose();
 		batch.dispose();
